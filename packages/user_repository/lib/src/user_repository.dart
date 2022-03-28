1c5ffc99b4af8baaf83af2628eefe0599e7b8d14
Kenneth Terrago <kterrago091893@gmail.com>
2022-03-28 14:23:58 +0800

    feat: add user_repository

---
+++ packages/user_repository/lib/src/user_repository.dart
@@ -0,0 +1,16 @@
+import 'package:uuid/uuid.dart';
+
+import 'models/models.dart';
+
+class UserRepository {
+  User? _user;
+
+  Future<User?> getUser() async {
+    if (_user != null) return _user;
+
+    return Future.delayed(
+      Duration(microseconds: 300),
+      () => _user = User(const Uuid().v4()),
+    );
+  }
+}
